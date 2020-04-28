4c4a9d32429080599e506a976a99f7a4f903476f
User1 <user@example.com>
2020-04-28 14:47:31 -0500

    1

--- map3/map3.js
+++ map3/map3.js
@@ -1,3 +1,4 @@
+/* global L */
 var map3 = L.map('map3').setView([31, -100], 4)
 var statesLayerObject = L.layerGroup().addTo(map3)
 var grayBasemapObject = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}').addTo(map3)
