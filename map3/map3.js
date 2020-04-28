8dede31c213d3fa379233d28a3be5a2f98546761
User1 <user@example.com>
2020-04-28 14:34:16 -0500

    !

--- map3/map3.js
+++ map3/map3.js
@@ -1,36 +1,36 @@
-var map3 = L.map('map3').setView([32.18, -99.14], 4)
+var map3 = L.map('map3').setView([31, -100], 4)
 var statesLayerObject = L.layerGroup().addTo(map3)
 var grayBasemapObject = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}').addTo(map3)
-var renameThisUrl = 'https://geog4046.github.io/assignment-resources/data/us_state_demographics_ESRI_2010A.geojson'
+var URL = 'https://geog4046.github.io/assignment-resources/data/us_state_demographics_ESRI_2010A.geojson'
 var streetsBasemapObject = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}').addTo(map3)
 var basemapsObject = {
-  'Streets': streetsBasemapObject,
+  'streets': streetsBasemapObject,
   'Gray canvas': grayBasemapObject
 }
-jQuery.getJSON(renameThisUrl, function (data) {
-  var renameThisStyleFunction = function (feature) {
-    var renameThisAgeInteger = feature.properties.MED_AGE 
-    var renameThisColorString = 'olive'
-    if (renameThisAgeInteger < 38) { renameThisColorString = 'green' }
+jQuery.getJSON(URL, function (data) {
+  var map3style = function (feature) {
+    var medpop = feature.properties.POPULATION
+    var map3color = 'olive'
+    if (medpop < 6458430) { map3color = 'green' }
     return {
-      color: renameThisColorString,
+      color: map3color,
       weight: 1,
       fillOpacity: 0.5
     }
   }
-  var renameThisGeojsonOptionsObject = {
-    style: renameThisStyleFunction,
-    onEachFeature: renameThisOnEachFeatureFunction
+  var popup = {
+    style: map3style,
+    onEachFeature: feature1
   }
-  L.geoJSON(data, renameThisGeojsonOptionsObject).addTo(map3
+  L.geoJSON(data, popup).addTo(map3)
 })
-var renameThisOnEachFeatureFunction = function (feature, layer) {
+var feature1 = function (feature, layer) {
   var name = feature.properties.STATE_NAME
-  var age = feature.properties.MED_AGE
-  layer.bindPopup('Median age of ' + name + ': ' + age + '<br>National average: 38')
+  var population = feature.properties.POPULATION
+  layer.bindPopup('Median population of ' + name + ': ' + population + '<br>National average: 6458430')
   statesLayerObject.addLayer(layer)
 }
 var layersObject = {
-  'Median age by state': statesLayerObject
+  'Median population by state': statesLayerObject
 }
 L.control.layers(basemapsObject, layersObject).addTo(map3)
